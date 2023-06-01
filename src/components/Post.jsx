40e44b8db6b15dc9331245a980e29f1570c10e47
imani.devs <imaniroberts.dev@gmail.com>
2023-05-31 21:18:54 -0400

    formatted date for clarity

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DeletePostButton from '../utils/DeletePostButton';
-// import {EditPostButton} from '../utils/EditPostButton';
 import { Link, useNavigate } from 'react-router-dom';
 
 const url = "https://mern-blog-api-he2o.onrender.com/posts";
 
-
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleDateString('en-US', {
+  month: '2-digit',
+  day: '2-digit',
+  year: 'numeric'
+});
 
 const Post = () => {
-  const [posts, setPosts] = useState([]);
+const [posts, setPosts] = useState([]);
 const navigation = useNavigate()
   useEffect(() => {
     const fetchPosts = async () => {
@@ -33,8 +37,8 @@ const navigation = useNavigate()
           <img src={post.image} alt={post.title} />
           <p>{post.content}</p>
           <p>By {post.author}</p>
-          <p>Created at: {post.createdAt}</p>
-          <p>Updated at: {post.updatedAt}</p>
+          <p>Created at: {formattedDate}</p>
+          <p>Updated at: {formattedDate}</p>
           <DeletePostButton post={post._id} />
        
       <button onClick={()=> navigation(`/edit-post/${post._id}`)}>Edit</button>
